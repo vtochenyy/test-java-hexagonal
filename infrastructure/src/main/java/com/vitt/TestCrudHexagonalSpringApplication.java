71e529e9ef58f9217ac1fb68b1cb188311fe0083
vitt <vitalytochenyy@gmail.com>
2024-06-15 00:48:06 +0300

    init

---
+++ infrastructure/src/main/java/com/vitt/TestCrudHexagonalSpringApplication.java
@@ -0,0 +1,11 @@
+package com.vitt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCrudHexagonalSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestCrudHexagonalSpringApplication.class, args);
+    }
+}
